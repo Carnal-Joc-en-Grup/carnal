db6ab2529e76d73462bdd57820f18eff06996a26
u1979277 <u1979277@campus.udg.edu>
2023-06-04 17:43:13 +0200

    mas rata

--- js/classes/rata.js
+++ js/classes/rata.js
@@ -1,15 +1,15 @@
 "use strict";
 
 const states = {
-  idle: 0,
-  jump: 1,
-  fall: 2,
-  land: 3,
-  attack: 4,
-  sneak: 5,
-  sneakAttack: 6,
-  damage: 7,
-  death: 8,
+    idle: 0,
+    jump: 1,
+    fall: 2,
+    land: 3,
+    attack: 4,
+    sneak: 5,
+    sneakAttack: 6,
+    damage: 7,
+    death: 8,
 };
 
 const SPEED = 250;
@@ -17,32 +17,33 @@ const JUMP_SPEED = 500;
 const SPRITE_SIZE = 500;
 
 export default class Rata extends Phaser.GameObjects.Sprite {
-  constructor(data, hp = 1) {
-    let { scene, x, y, texture, frame } = data;
-    super(scene, x, y, texture, frame);
-    // Variables del personatge
-    this.hitPoints = hp;
-    // this.actualState = states.idle;
-    this.isDead = false;
-    this.scene.add.existing(this);
-    this.scene.physics.world.enable(this);
-    this.setScale(0.25);
-  }
-  create() {
-    console.log("Create Rata");
-    this.scene.physics.world.enable(this);
-    this.body.setBounce(0.1); // Configurar el rebot del jugador
+    constructor(data, hp = 1) {
+        let { scene, x, y, texture, frame } = data;
+        super(scene, x, y, texture, frame);
+        // Variables del personatge
+        this.hitPoints = hp;
+        // this.actualState = states.idle;
+        this.isDead = false;
 
-     this.scene.anims.create({
-      key: "walk_rata",
-      frames: this.scene.anims.generateFrameNumbers("rat_walk", {
-        start: 0,
-        end: 1,
-      }),
-      frameRate: 5,
-    });
-  }
-  update() {
-    this.anims.play("walk_rata",true);
-  }
+        this.scene.add.existing(this);
+        this.scene.physics.world.enable(this);
+        this.setScale(0.25);
+    }
+    create() {
+        this.scene.physics.world.enable(this);
+        this.body.setBounce(0.1); // Configurar el rebot del jugador
+
+        this.scene.anims.create({
+            key: "walk_rata",
+            frames: this.scene.anims.generateFrameNumbers("rat_walk", {
+                start: 0,
+                end: 1,
+            }),
+            frameRate: 5,
+        });
+    }
+    update() {
+        this.body.setVelocityX(SPEED);
+        this.anims.play("walk_rata", true);
+    }
 }
