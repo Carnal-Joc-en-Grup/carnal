9316c025751be4a6fc20145800abc4dfd919d424
u1979277 <u1979277@campus.udg.edu>
2023-06-04 07:03:21 +0200

    Animacions + de tot

--- js/classes/carnal.js
+++ js/classes/carnal.js
@@ -2,7 +2,7 @@
 
 const states = {
   idle: 0,
-  jumping: 1,
+  jump: 1,
   fall: 2,
   land: 3,
   attack: 4,
@@ -12,42 +12,155 @@ const states = {
 
 const SPEED = 250;
 const JUMP_SPEED = 300;
+const SPRITE_SIZE = 500;
 
 export default class Carnal extends Phaser.GameObjects.Sprite {
   constructor(data, hp = 9) {
-    let {scene, x, y, texture, frame} = data;
+    let { scene, x, y, texture, frame } = data;
     super(scene, x, y, texture, frame);
-    // Variables personaje
+    // Variables del personatge
     this.hitPoints = hp;
     this.actualState = states.idle;
     this.isDead = false;
-
     this.scene.add.existing(this);
     this.scene.physics.world.enable(this);
     this.setScale(0.25);
-    // Ajustar la hitbox sin cambiar el tamaño
-    // let width, height, offsetX, offsetY = 250;
-    this.body.setSize(240, 240, true);
   }
   create() {
     console.log("Create Carnal");
     this.scene.physics.world.enable(this);
-    this.body.setBounce(0.1); // Configurar el rebote del jugador
-  }
-  preload() {
+    this.body.setBounce(0.1); // Configurar el rebot del jugador
+
+    // En Facu tenia lo que no està comentat i s'ha pirat a dormir quan es moix no se pot veure per lo que no tenc ni puta idea de si res funciona
+    // No se que ha tocat, perquè abans si que se podia veure. Jo he afeigit tot lo que està comentat a partir d'aquí, es a dir sa resta d'estats i ses seves animacions
+
+    //this.scene.anims.create({
+    //  key: "walk",
+    //  frames: this.scene.anims.generateFrameNumbers("carnal_walk", {
+    //    start: 0,
+    //    end: 4,
+    //  }),
+    //  frameRate: 8,
+    //  repeat: 0
+    //});
+    //this.scene.anims.create({
+    //  key: "attack",
+    //  frames: this.scene.anims.generateFrameNumbers("carnal_attack", {
+    //    start: 0,
+    //    end: 3,
+    //  }),
+    //  frameRate: 8,
+    //  repeat: 0
+    //});
+    //this.scene.anims.create({
+    //  key: "sneak",
+    //  frames: this.scene.anims.generateFrameNumbers("carnal_sneak", {
+    //    start: 0,
+    //    end: 2,
+    //  }),
+    //  frameRate: 8,
+    //  repeat: 0
+    //});
+    //this.scene.anims.create({
+    //  key: "sneak_attack",
+    //  frames: this.scene.anims.generateFrameNumbers("carnal_sneak_attack", {
+    //    start: 0,
+    //    end: 2,
+    //  }),
+    //  frameRate: 8,
+    //  repeat: 0
+    //});
+    this.scene.anims.create({
+      key: "jump",
+      frames: this.scene.anims.generateFrameNumbers("carnal_jump", {
+        start: 0,
+        end: 3,
+      }),
+      frameRate: 8,
+      repeat: 0
+    });
+    this.scene.anims.create({
+      key: "fall",
+      frames: this.scene.anims.generateFrameNumbers("carnal_jump", {
+        start: 4,
+        end: 4,
+      }),
+      frameRate: 8,
+    });
+    this.scene.anims.create({
+      key: "land",
+      frames: this.scene.anims.generateFrameNumbers("carnal_jump", {
+        start: 5,
+        end: 7,
+      }),
+      frameRate: 8,
+      repeat: 0
+    });
+    this.scene.anims.create({
+      key: "idle",
+      frames: this.scene.anims.generateFrameNumbers("carnal_wait", {
+        start: 0,
+        end: 0,
+      }),
+      frameRate: 0,
+    });
   }
   update() {
     if (this.scene.inputKeys.left.isDown) {
-        this.setFlipX(true);
-        this.body.setVelocityX(-SPEED);
+      this.setFlipX(true);
+      this.body.setVelocityX(-SPEED);
     } else if (this.scene.inputKeys.right.isDown) {
-        this.setFlipX(false);
-        this.body.setVelocityX(SPEED);
+      this.setFlipX(false);
+      this.body.setVelocityX(SPEED);
     } else {
-        this.body.setVelocityX(0);
+      this.body.setVelocityX(0);
     }
-      if (this.scene.inputKeys.jump.isDown && this.body.onFloor()) { 
-        this.body.setVelocityY(-JUMP_SPEED);
+    if (this.scene.inputKeys.jump.isDown && this.body.onFloor()) {
+      this.actualState = states.jump;
+      this.body.setVelocityY(-JUMP_SPEED);
+    } else if (this.body.onFloor()) {
+      this.actualState = states.idle;
+    }
+    
+    switch (this.actualState) {
+      case states.idle:
+        this.anims.play("idle", true);
+        break;
+      //case states.walk:
+      //  this.anims.play("walk", true);
+      //  break;
+      //case states.attack:
+      //  this.anims.play("attack", false);
+      //  break;
+      //case states.sneak:
+      //  this.anims.play("sneak", true);
+      //  break;
+      //case states.sneakAttack:
+      //  this.anims.play("sneak_attack", false);
+      //  break;
+      case states.jump:
+        this.anims.play("jump", true);
+        this.on('ANIMATION_COMPLETE', () => {
+          console.log('AnimationComplete!!!');
+          this.actualState = states.fall;
+        })
+        break;
+      case states.fall:
+        this.anims.play("fall", false);
+        break;
+      //case states.land:
+      //  this.anims.play("land", false);
+      //  break;
+    }
+  }
+  changeHitbox() {
+    if (this.actualState == 5 || this.actualState == 6) {
+      // Sneak
+      this.body.setSize(240, 350, true);
+      this.body.setOffset(125, 125);
+    } else {
+      this.body.setSize(300, 300, true);
+      this.body.setOffset(125, 150);
     }
   }
 }
--- js/scenes/platformgame.js
+++ js/scenes/platformgame.js
@@ -2,11 +2,11 @@
 import Carnal from "../classes/carnal.js";
 
 var config = {
-    width: 800,
-    height: 728
+  width: 800,
+  height: 728,
 };
 
-const MAP_WIDTH = 950+config.width;
+const MAP_WIDTH = 950 + config.width;
 const MAP_HEIGHT = 728;
 
 export default class PlatformScene extends Phaser.Scene {
@@ -23,65 +23,95 @@ export default class PlatformScene extends Phaser.Scene {
   }
   preload() {
     // Carnal.preload(this);
-    this.load.image("herb", "../resources/props/herba.png");
-    this.load.image("background", "../../resources/backgrounds/background_passadisPorta.jpg");
+    //Backgrounds
+    
+    this.load.image("background1", "../../resources/backgrounds/background_passadisPorta.jpg");
+    this.load.image("background2", "../../resources/backgrounds/background_passadis.jpg");
+    this.load.image("background3", "../../resources/backgrounds/background_ventilacio.jpg");
+    this.load.image("background4", "../../resources/backgrounds/background_pati.jpg");
+
+    // Props
+    this.load.image("entrada", "../resources/props/entrada.png");
     this.load.image("tuberia_tileset", "../../resources/props/tuberia_tileset.png");
+    this.load.image("herb", "../resources/props/herba.png");
+    this.load.image("box", "../resources/props/box.png");
+
+    // Icones
+    this.load.image("cor", "../resources/icones/cor.png");
+    this.load.image("rata", "../resources/icones/rata.png");
+    this.load.image("herba", "../resources/icones/herba.png");
+
+
     this.load.image("Collision", "../../resources/assets/Collision.png");
+    this.load.tilemapTiledJSON("TileMap001", "../../tiled/TileMap001.json");
+    
+    // Player
+    this.load.spritesheet("carnal_walk", "../../resources/carnal_sprites/carnal_walk.png", {frameWidth: 500, frameHeight: 500});
+    this.load.spritesheet("carnal_attack", '../../resources/carnal_sprites/carnal_attack.png', {frameWidth: 500, frameHeight: 500});
+    this.load.spritesheet("carnal_sneak", '../../resources/carnal_sprites/carnal_sneak.png', {frameWidth: 500, frameHeight: 500});
+    this.load.spritesheet("carnal_sneak_attack", '../../resources/carnal_sprites/carnal_sneak_attack.png', {frameWidth: 500, frameHeight: 500});
+    this.load.spritesheet("carnal_jump", '../../resources/carnal_sprites/carnal_jump.png', {frameWidth: 500, frameHeight: 500});
+    this.load.spritesheet("carnal_wait", '../../resources/carnal_sprites/carnal_wait.png', {frameWidth: 500, frameHeight: 500}); 
     this.load.image("carnal-texture", "../../resources/prueba.png");
 
-    this.load.tilemapTiledJSON("TileMap001", "../../tiled/TileMap001.json");
+    // Personatge
+    this.load.spritesheet("paloma_idle", '../../resources/paloma_sprites/paloma_idle.png', {frameWidth: 500, frameHeight: 500}); 
+
+    // Enemics
+    this.load.spritesheet("rat_walk", '../../resources/rats_sprites/rat_walk.png', {frameWidth: 343, frameHeight: 142});
   }
+
   create() {
-	// Scene Backgorund
-	let bg = this.add.image(config.width/2, config.height/2, 'background');
-	bg.setScale(0.675);
-	//bg.setScrollFactor(0);
+    // Scene Backgorund
+    let bg = this.add.image(config.width / 2, config.height / 2, "background1");
+    bg.setScale(0.675);
+    //bg.setScrollFactor(0);
     // create the Tilemap
     const map = this.make.tilemap({
-		key: "TileMap001"
-
-	});
-    const tilesetTuberias = map.addTilesetImage("tuberia_tileset");
+      key: "TileMap001",
+    });
+    const tilesetTuberies = map.addTilesetImage("tuberia_tileset");
     const tilesetCollision = map.addTilesetImage("Collision");
 
-    const layerTuberias = map.createLayer("Tiles", tilesetTuberias);
-	const layerCollision = map.createLayer("Collisions", tilesetTuberias);
-	layerTuberias.setScale(0.2);
-	layerCollision.setScale(0.2);
-
-	
-    this.inputKeys = this.input.keyboard.addKeys({
-		up: Phaser.Input.Keyboard.KeyCodes.UP,
-		left: Phaser.Input.Keyboard.KeyCodes.LEFT,
-		right: Phaser.Input.Keyboard.KeyCodes.RIGHT,
-		attack: Phaser.Input.Keyboard.KeyCodes.S,
-		interact: Phaser.Input.Keyboard.KeyCodes.D,
-		jump: Phaser.Input.Keyboard.KeyCodes.SPACE,
-		sneak: Phaser.Input.Keyboard.KeyCodes.A,
-	});
+    const layerTuberies = map.createLayer("Tiles", tilesetTuberies);
+    const layerCollision = map.createLayer("Collisions", tilesetCollision);
+    layerTuberies.setScale(0.2);
+    layerCollision.setScale(0.2);
+    
+    //En Facu havia llevat aquest tros i no es veia es moix per això, no se perquè putes ho lleva
     this.player = new Carnal({
-      scene: this, // Pasa el objeto de escena actual
-      x: 0,
-      y: 0,
+      scene: this, // Passa l'objecte a l'escena actual
+      x: 100,
+      y: 610,
       texture: "carnal-texture",
       frame: "carnal-frame",
+    })
+
+    this.inputKeys = this.input.keyboard.addKeys({
+      left: Phaser.Input.Keyboard.KeyCodes.A,
+      right: Phaser.Input.Keyboard.KeyCodes.D,
+      attack: Phaser.Input.Keyboard.KeyCodes.E,
+      interact: Phaser.Input.Keyboard.KeyCodes.Q,
+      jump: Phaser.Input.Keyboard.KeyCodes.W,
+      sneak: Phaser.Input.Keyboard.KeyCodes.S,
+      pause: Phaser.Input.Keyboard.KeyCodes.ESC
     });
+    
     this.player.create();
 
-	// Colisiones
-	this.physics.add.collider(this.player, layerTuberias);
-	this.physics.add.collider(this.player, layerCollision);
-	layerTuberias.setCollisionBetween(5,10);
-	layerCollision.setCollisionBetween(10,12);
+    // Colisions
+    this.player.changeHitbox();
+    this.physics.add.collider(this.player, layerTuberies);
+    this.physics.add.collider(this.player, layerCollision);
+    layerTuberies.setCollisionBetween(5, 10);
+    layerCollision.setCollisionBetween(10, 12);
 
-	this.cameras.main.setBounds(0, 0, MAP_WIDTH, MAP_HEIGHT); // Ajusta los límites de la cámara según el tamaño de tu escena
-	this.cameras.main.startFollow(this.player, true, 0.5, 0.5); // Establece a Carnal como el objeto a seguir en la cámara
-  
+    this.cameras.main.setBounds(0, 0, MAP_WIDTH, MAP_HEIGHT); // Ajusta els límits de la càmera segons el tamany de l'escena
+    this.cameras.main.startFollow(this.player, true, 0.5, 0.5); // Estableix a Carnal com a l'objecte a seguir amb la càmara
   }
   update() {
     if (this.gameOver) return;
     this.player.update();
-	console.log(this.cameras.main.scrollX)
   }
   collectStar(player, star) {
     star.disableBody(true, true);
